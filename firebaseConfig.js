e935047beb99bfeeb159e7c8b1ed722f9afcfd33
MiliXsoft <git.konto8@gmail.com>
2025-11-02 14:09:56 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,4 +1,5 @@
-const firebaseConfig = {
+// Upewnij się, że plik jest w tym samym folderze co index.html
+export const firebaseConfig = {
   apiKey: "AIzaSyAzi6XK8OL8Fr1xLb-pyQw2mMPuI1-g4iM",
   authDomain: "milixsoft-9b81e.firebaseapp.com",
   projectId: "milixsoft-9b81e",
@@ -6,5 +7,3 @@ const firebaseConfig = {
   messagingSenderId: "443043131107",
   appId: "1:443043131107:web:1ff1926eff9ada03dedfd6"
 };
-
-export default firebaseConfig;
