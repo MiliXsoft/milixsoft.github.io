96e69aa081d1bd2b470fd001e4a0b94d9ddd9a9a
MiliXsoft <git.konto8@gmail.com>
2025-11-02 14:15:03 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,11 @@
-// Upewnij się, że plik jest w tym samym folderze co index.html
-export const firebaseConfig = {
+// firebaseConfig.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
+// ⬅️ Wstaw tu swoje dane
+const firebaseConfig = {
   apiKey: "AIzaSyAzi6XK8OL8Fr1xLb-pyQw2mMPuI1-g4iM",
   authDomain: "milixsoft-9b81e.firebaseapp.com",
   projectId: "milixsoft-9b81e",
@@ -7,3 +13,9 @@ export const firebaseConfig = {
   messagingSenderId: "443043131107",
   appId: "1:443043131107:web:1ff1926eff9ada03dedfd6"
 };
+
+// Inicjalizacja
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
