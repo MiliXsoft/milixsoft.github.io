c7f40057eb8a2470eeb3a701cb3d76ba4c8fe475
MiliXsoft <git.konto8@gmail.com>
2025-11-02 14:20:31 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,13 +9,16 @@ const firebaseConfig = {
   apiKey: "AIzaSyAzi6XK8OL8Fr1xLb-pyQw2mMPuI1-g4iM",
   authDomain: "milixsoft-9b81e.firebaseapp.com",
   projectId: "milixsoft-9b81e",
-  storageBucket: "milixsoft-9b81e.firebasestorage.app",
+  storageBucket: "milixsoft-9b81e.appspot.com", // ✅ poprawiony adres storageBucket
   messagingSenderId: "443043131107",
   appId: "1:443043131107:web:1ff1926eff9ada03dedfd6"
 };
 
 // Inicjalizacja
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+// Eksporty
+export { app, auth, db, storage };
