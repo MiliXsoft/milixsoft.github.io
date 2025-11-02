9a070c8636bf7c81ca9758cc7e3095704faa264c
MiliXsoft <git.konto8@gmail.com>
2025-11-02 14:28:56 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,17 +4,17 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
-// ⬅️ Wstaw tu swoje dane
+// Konfiguracja Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAzi6XK8OL8Fr1xLb-pyQw2mMPuI1-g4iM",
   authDomain: "milixsoft-9b81e.firebaseapp.com",
   projectId: "milixsoft-9b81e",
-  storageBucket: "milixsoft-9b81e.appspot.com", // ✅ poprawiony adres storageBucket
+  storageBucket: "milixsoft-9b81e.appspot.com", // ✅ poprawiony adres
   messagingSenderId: "443043131107",
   appId: "1:443043131107:web:1ff1926eff9ada03dedfd6"
 };
 
-// Inicjalizacja
+// Inicjalizacja Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
